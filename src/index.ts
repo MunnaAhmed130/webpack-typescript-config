75fe7b3142de54841ddb232387fd7cb16ee9f210
Munna Ahmed <munnaahmed2025@gmail.com>
2024-03-01 15:10:21 +0600

    entry point

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,18 @@ const sum = (num1: number, num2: number): number => {
 };
 
 console.log(sum(4, 8));
+
+const obj = {
+  one: {
+    two: {
+      three: "hello",
+    },
+  },
+};
+
+function speak(noise: any): void {
+  console.log(noise?.one?.two?.three);
+  console.log(noise && noise.one && noise.one.two && noise.one.two.three);
+}
+
+speak(obj);
